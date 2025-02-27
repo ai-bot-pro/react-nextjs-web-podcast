d672b0e046d9f26a57b4118226f5a2367623083d
weedge <weege007@gmail.com>
2025-02-27 15:46:40 +0800

    change close
    Signed-off-by: weedge <weege007@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,18 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import NextScript from "next/script";
 import Script from "next/script";
-
+import { Metadata } from "next";
 import { Body } from "@/layouts/body";
 import { AudioContextProvider } from "@/contexts/AudioContext";
+
+export const metadata: Metadata = {
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    minimumScale: 1,
+    maximumScale: 1,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -16,7 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-
         <NextScript strategy="afterInteractive" />
         <Script
           id="google-analytics"
@@ -36,7 +44,6 @@ export default function RootLayout({
           }}
         />
       </head>
-
       <Body>
         <AudioContextProvider>
           <Toaster position="top-center" richColors />
